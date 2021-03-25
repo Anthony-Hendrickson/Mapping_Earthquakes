ae2e473c5decb7bed8a6a9d4568fcd3160a1df0e
Tony <anthonyhendrickson7@gmail.com>
2021-03-25 13:41:40 -0400

    13.5.5.2

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -22,23 +22,22 @@ let sanFranAirport =
 ]};
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://github.com/Anthony-Hendrickson/Mapping_Earthquakes/blob/main/torontoRoutes.json";
+
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-    //We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-        console.log(feature);
-        console.log(layer);
-        layer.bindPopup();
-    }
-}).addTo(map);
-
-
+d3.json(torontoData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
